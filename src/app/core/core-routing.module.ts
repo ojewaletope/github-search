33976215990ee6fda9d0ef026fcc1aa92771175d
topeojewale <topeapps@gmail.com>
2021-07-07 01:46:32 +0100

    initial set up

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SearchComponent} from "./components/search/search.component";
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./core/core.module').then(m => m.CoreModule)
+    component: SearchComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class CoreRoutingModule { }
